9492ce679c9785c6c8ed220c3db2cb15786c2674
bryan kennedy <bkennedy@smm.org>
2014-07-29 15:48:47 -0500

    Adding initial templates for COmponent view

---
+++ client/views/components/components.js
@@ -0,0 +1,9 @@
+/**
+ * Helpers for the Components List
+ *
+ * Right now this is simple console log to let us know that things are working.
+ */
+Template.componentsList.rendered = function() {
+    console.log('componentsList this - ', this);
+};
+
